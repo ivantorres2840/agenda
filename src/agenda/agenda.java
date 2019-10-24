d51041df2d2ef2aed25530003770e89026b2b198
alumno <alumno@aula1-10>
2019-10-24 14:33:57 +0200

    agendita

---
+++ src/agenda/agenda.java
@@ -0,0 +1,120 @@
+package agenda;
+
+import java.util.Scanner;
+
+public class agenda {
+
+	public static int numeros1() {
+		int pepe = 0;
+		boolean numero = false;
+		Scanner leer;
+		
+		do {
+			System.out.println("1. Guardar Contacto");
+			System.out.println("2. Borrar Contacto");
+			System.out.println("3. Ver Contacto");
+			System.out.println("4. Buscar Contacto");
+			System.out.println("5. Editar Contacto");
+			System.out.println("6. Salir");
+			
+			leer = new Scanner(System.in);
+		pepe = leer.nextInt();
+		
+			if ((pepe > 0) && (pepe<7)) {
+				numero = true;
+			}
+		}while(!numero);
+		return pepe;
+	}
+	
+	public static void guardarcontactos(String magenda[][]) {
+		Scanner leer;
+		leer = new Scanner(System.in);
+		int numeroposicion=posicion(magenda);
+		
+				System.out.println("introduce nombre y edad");
+				magenda[numeroposicion][0]=leer.nextLine();
+				magenda[numeroposicion][1]=leer.nextLine();
+		}
+			
+		
+	
+	
+	public static int posicion(String magenda[][]) {
+		int posi=0;
+	for(int i=0;i<magenda.length;i++) {
+		 
+			if (magenda[i]==null){
+					posi=i;
+				break;
+			}
+		
+	}
+	return posi;
+	}
+	
+	public static void vercontactos(String magenda[][]) {
+		for (int i=0;i<magenda.length;i++) {
+			if (magenda[i][0]!=null) {
+				System.out.println(magenda[i][0]+" "+magenda[i][1]);
+			}
+	}
+			System.out.println(" ");
+		}
+	
+	
+	public static void buscarcontactos(String magenda[][]) {
+		Scanner leer;
+		leer = new Scanner(System.in);
+		String nombre = "";
+		
+		System.out.println("A quien quieres buscar");
+		nombre = leer.nextLine();
+		for (int i=0; i<magenda.length;i++) {
+			if ((magenda[i][0]!=null) && (magenda[i][0].equalsIgnoreCase(nombre))){
+				System.out.println(magenda[i][0] + magenda[i][1]) ;
+				
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int seleccion = 0;
+		String magenda[][] = new String [2][2];
+		
+		
+		
+		do {
+		seleccion=numeros1();
+		switch(seleccion) {
+		case 1:
+			guardarcontactos(magenda);
+			break;
+		case 2:
+			//borrarcontactos(magenda);
+			break;
+		case 3:
+			vercontactos(magenda);
+			break;
+		case 4:
+			buscarcontactos(magenda);
+			break;
+		case 5:
+			//editarcontacos(magenda);
+			break;
+		case 6:
+			System.out.println("Adios");
+			break;
+		}
+		}while(seleccion!=6);
+		posicion(magenda);
+	}
+	
+}
+
